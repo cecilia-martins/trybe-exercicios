1cdd2afbf793cd812deda8b7beb3a825256cdb95
Cecília Martins <cecilia_lmartins@hotmail.com>
2021-11-29 18:50:16 -0300

    exercicios do dia 2 do bloco 4 - ARRAY E FOR

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-loop-for/array-e-for-4.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (var posicao = 0; posicao < numbers.length; posicao+=1) {
+     soma = soma + numbers[posicao];  
+}
+
+if(soma / numbers.length > 20){
+    console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor ou igual a 20");
+}
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-loop-for/array-e-for-5.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (var posicao = 0; posicao < numbers.length; posicao+=1) {
+     soma = soma + numbers[posicao];  
+}
+
+    console.log(soma);
