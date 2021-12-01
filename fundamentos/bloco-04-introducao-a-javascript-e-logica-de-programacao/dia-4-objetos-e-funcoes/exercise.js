dd2a7c3818d0b33d50879e4230bf6828cf9ed0ac
Cecília Martins <cecilia_lmartins@hotmail.com>
2021-12-01 17:40:11 -0300

    exercicios de objetos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/exercise.js
@@ -0,0 +1,12 @@
+let diasDaSemana = {
+    : 'domingo',
+    : 'segunda',
+    : 'terça',
+    : 'quarta',
+    : 'quinta',
+    : 'sexta',
+    : 'sábado',
+    };
+    
+    diasDaSemana.3; // SyntaxError: Unexpected number
+    console.log(diasDaSemana['1']); // domingo
