44c14f615ae37c5dcd88238583750ced793069ba
Cecília Martins <cecilia_lmartins@hotmail.com>
2021-12-06 09:57:44 -0300

    REVISANDO BLOCO 4 - primeiros passos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/operadores.js
@@ -0,0 +1,46 @@
+
+let a = 0; //NUMBER
+let b = "Gian"; //STRING
+let c = true; //BOOLEN
+let d;        //UNDERFINED
+let e = null; //NULL
+
+let isItFriday = true;
+
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+
+//SOMA
+let soma = num1 + num2;
+console.log(soma);
+
+//SUBTRAÇÃO
+let subtracao = num1 - num2;
+console.log(subtracao);
+
+//DIVISÃO
+
+let divisao = num1 / num2;
+console.log(divisao);
+
+//MULTIPLICAÇÃO
+
+let multiplicacao = num1 * num2;
+console.log(multiplicacao);
+
+let resultado = 3 + 4 * 5;
+console.log(resultado);
+
+//COMPARAÇÃO
+let comparacao = num1 > num2;
+console.log(comparacao);
+
+
+
+//SABER SE É PAR OU IMPAR?
+let isEqual = num1 === num2;
+console.log(isEqual);
+
+let isEven = ((num1 +num2) % 2) == 0;
+console.log(isEven)
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/script.js
@@ -0,0 +1,11 @@
+
+//let declara uma variavel
+//o const um valor fixo
+
+let variavel = 10;
+const constante = "fixo";
+
+variavel = 20;
+
+
+console.log("Turma 19 - tribo C adora js");
