d4ea095200a97325955bf4e3b48070ac9771b76e
Cecília Martins <cecilia_lmartins@hotmail.com>
2021-12-06 10:10:26 -0300

    REVISANDO BLOCO 4 - primeiros passos - 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/operadores.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/operadores.js
@@ -44,3 +44,9 @@ console.log(isEqual);
 
 let isEven = ((num1 +num2) % 2) == 0;
 console.log(isEven)
+
+if (num1 > num2) { 
+    console.log("num1 wins");
+} else {
+    console.log("num2 wins");
+}
