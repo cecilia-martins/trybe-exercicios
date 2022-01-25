1f574d54403eadb99c16316f8ae8827479c7997f
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-01-25 17:44:07 -0300

    adicionado alguns exercicios do 5.1 - o css do adm de tempo não está funcionando. Verificar depois

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-DOM-e-seletores/exercicio - Administrador de Tempo da Trybe/script.js
@@ -0,0 +1,15 @@
+//pegando a emergency-tasks
+let urgente = document.querySelector('.emergency-tasks');
+tarefasUrgente.style.backgroundColor = "blueviolet";
+
+//pegando a no-emergency-tasks
+let noUrgent = document.querySelector('.no-emergency-tasks');
+noUrgent.style.backgroundColor = "black";
+
+// pegando o cabeçalho
+let cabeçalho = document.getElementById('header-container');
+cabeçalho.style.backgroundColor = "mediumseagreen";
+
+//dando cor ao urgente
+let corFundo1 = document.getElementById('corUrgente');
+corFundo1.style.backgroundColor ="salmon";
