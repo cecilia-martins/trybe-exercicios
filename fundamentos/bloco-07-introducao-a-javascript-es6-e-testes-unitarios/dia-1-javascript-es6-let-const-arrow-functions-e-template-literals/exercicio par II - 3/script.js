e8644b4cfe0ee3390839977788ccf33e8d34c62f
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-01-29 18:37:02 -0300

    adicionado exercicio 7.1 - pela metade pois sou tão inteligente quanto uma porta :clow_face:

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio par II - 3/script.js
@@ -0,0 +1,12 @@
+const procuraX = (paramString) => {
+
+let x = "";
+  for  (let posi = 0; posi < paramString.lenght; posi+= 1) {
+    if (paramString[posi] == "x") {
+      x = "Bebeto"
+      return ``
+    }
+  }
+}
+console.log(procuraX('Tryber x aqui!'));
+//////////////TUDO ERRADO
