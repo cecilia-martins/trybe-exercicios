6d724954dd93345ab5191baa05dae8ae4b57db03
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-04 17:30:16 -0300

    exercicios incompletos, porem add, do dia 8.1 pois sou burra :)

---
+++ fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/exercicio bonus - 8.1.js
@@ -0,0 +1,3 @@
+//Nestes exercícios você irá implementar HOFs que simulam um turno de batalha em um jogo. Você irá criar funções que calculam dano, atualizam status, e ao final, retornam os resultados da rodada.
+//Para os próximos exercícios copie o código abaixo.
+
