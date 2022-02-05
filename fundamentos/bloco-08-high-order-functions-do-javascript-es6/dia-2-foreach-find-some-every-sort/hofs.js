b2765bdafc00ca795c2f7dc842fa66de9468ad90
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-05 09:11:39 -0300

    adicionado algumas testes, para fixar, etc

---
+++ fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-2-foreach-find-some-every-sort/hofs.js
@@ -0,0 +1,85 @@
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+// students é um objeto
+// a função verify passa por cada um dos students...
+function verifyGrades() {
+  for (let i = 0; i < students.length; i += 1) {
+    //atribui cada um no student/singular!
+    const student = students[i];
+    //se esse student tiver nota maior ou igual a 60...
+    if (student.grade >= 60) {
+      //adiciona a ele o aprovado
+      student.approved = 'Aprovado';
+    } else {
+      student.approved = 'Recuperação'; //aqui é o senão
+    }
+  }
+}
+
+verifyGrades(); //aqui chama
+
+console.log(students); //aqui printa
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
+
+
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+// MESMA COISA DO ANTERIOR SO QUE USANDO O FOREACH
+function verifyGrades() {
+  //PARA CADA STUDENT...ETC
+  students.forEach((student, index) => {
+    if (student.grade >= 60) {
+      students[index].approved = 'Aprovado';
+    } else {
+      students[index].approved = 'Recuperação';
+    }
+  });
+}
+
+verifyGrades();
+
+console.log(students);
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
+
+
+
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+let firstMultipleOf5;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 5 === 0) {
+    firstMultipleOf5 = numbers[index];
+    break;
+  }
+}
+
+console.log(firstMultipleOf5);
+// 50
+
+
+
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+console.log(firstMultipleOf5);
+// 50
+
+
+
