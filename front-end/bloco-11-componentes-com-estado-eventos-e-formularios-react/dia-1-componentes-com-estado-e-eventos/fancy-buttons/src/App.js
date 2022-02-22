29dd4a0259d8d0550a83355012a7c1c2a56b0cdc
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-22 20:09:02 -0300

    Adicionando exercicios - não completados ou só anotações

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -0,0 +1,20 @@
+    // // src/App.js
+    // import React from 'react';
+
+    
+
+    // class App extends React.Component {
+    //   constructor() {
+    //     super();
+    //     this.handleClick = this.handleClick.bind(this);
+
+    //     handleClick() {
+    //   console.log('alguma coisa')
+    // }
+    //   }
+    //   render() {
+    //     return (<button onClick={ this.handleClick }>Botão</button>);
+    //   }
+    // }
+
+    // export default App;
