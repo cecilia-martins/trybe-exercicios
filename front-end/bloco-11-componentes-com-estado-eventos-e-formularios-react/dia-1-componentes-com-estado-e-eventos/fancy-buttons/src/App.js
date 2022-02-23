0163a112721fe4c432e0bcbbe9c40278aa5fb902
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-23 19:32:05 -0300

    add algumas alterações

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,20 +1,14 @@
-    // // src/App.js
-    // import React from 'react';
+    // src/App.js
+    import React from 'react';
 
-    
+    function handleClick() {
+      console.log('Clicou no botão!');
+    }
 
-    // class App extends React.Component {
-    //   constructor() {
-    //     super();
-    //     this.handleClick = this.handleClick.bind(this);
+    class App extends React.Component {
+      render() {
+        return (<button onClick={ handleClick }>Botão</button>);
+      }
+    }
 
-    //     handleClick() {
-    //   console.log('alguma coisa')
-    // }
-    //   }
-    //   render() {
-    //     return (<button onClick={ this.handleClick }>Botão</button>);
-    //   }
-    // }
-
-    // export default App;
+    export default App;
