ae1a3498ea5ba88f6cc5c3b9ad9cb74dc99d98b7
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-06-07 14:52:23 -0300

    :sparkles: feat: primeiro hello world com nodejs

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-intro-nodejs/hello-world/index.js
@@ -0,0 +1,7 @@
+// console.log('Hello World');
+const readline = require('readline-sync');
+
+const name = readline.question('Whats your name?');
+const age = readline.questionInt('How old are you?');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
