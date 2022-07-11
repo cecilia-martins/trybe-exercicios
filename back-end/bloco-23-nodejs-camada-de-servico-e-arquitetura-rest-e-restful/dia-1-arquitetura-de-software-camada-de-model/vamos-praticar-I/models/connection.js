b6ae4a58f27494b00d8357d5c11e6ff13b74922b
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-07-11 18:30:22 -0300

    :sparkles: feat: Adicionado + vamos praticar

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/vamos-praticar-I/models/connection.js
@@ -0,0 +1,9 @@
+// const ttt = require('mysql2/promise');
+
+// const connection = ttt.createPool({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'senha123',
+// 	database: 'model_example' });
+
+// module.exports = connection;
