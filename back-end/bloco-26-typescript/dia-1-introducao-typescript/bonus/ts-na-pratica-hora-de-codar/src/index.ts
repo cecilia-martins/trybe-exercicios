00ab5f2beb9148808cac6d04f9b0b4e500e76cda
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-07-26 12:49:04 -0300

    :sparkles: feat: Adicionado anotações/atividades

---
+++ back-end/bloco-26-typescript/dia-1-introducao-typescript/bonus/ts-na-pratica-hora-de-codar/src/index.ts
@@ -0,0 +1,12 @@
+// const x = 10;
+let x: number = 10 // string, boolean, number, etc...
+
+// x = "teste" // o tipo string não pode ser atribuido ao tipo number
+console.log(x);
+
+// inferencia Vs annotation
+
+let y = 12; // inferencia
+let z: number = 12; // annotation
+
+// tipos basicos
