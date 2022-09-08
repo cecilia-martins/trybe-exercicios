4fba68c90fad336a4e4ee145f9ca32f4c589364f
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-09-08 18:48:16 -0300

    :sparkles: feat: introdução ao Ts - Dio

---
+++ back-end/secao-8-typescript/dia-1-introducao-ao-typescript/bonus/introducao-ao-ts-dio/index.ts
@@ -0,0 +1,11 @@
+function somar(num: number) {
+  return num + 1;
+}
+
+console.log(somar(2));
+
+// function greeting(num: number, name: string) {
+//   return `Bem vinde ${name}, seu número é: ${num}`
+// }
+
+// console.log(greeting(2, 'fulana'));
