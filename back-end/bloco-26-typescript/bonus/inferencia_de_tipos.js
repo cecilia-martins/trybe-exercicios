6566794b7f1674a00d351eaec89a245c73721bfd
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-09-09 17:47:56 -0300

    :sparkles: feat: Adicionando anotações/bonus

---
+++ back-end/bloco-26-typescript/bonus/inferencia_de_tipos.js
@@ -0,0 +1,9 @@
+var spaceship = {
+    name: 'string',
+    speed: 0 // att o tipo number sem dizer que é number
+};
+function accelerate(spaceshipt, speed) {
+    spaceship.speed = speed;
+}
+;
+accelerate(spaceship, 50);
---
+++ back-end/bloco-26-typescript/bonus/tuplas.ts
@@ -0,0 +1,13 @@
+// let crew: string[];
+// crew.push('string1')
+
+// let crew: [string, string, string]; // recebe especificamente 3 strings
+// crew[0] = 'nome';
+// crew[1] = 'nome';
+// crew[2] = 'nome';
+// crew[3] = 'nome'; // no quarto daria erro, pois ele só recebe 3 strings
+
+let point: [number, number, string, boolean ];
+
+point = [2, 5, 'asjda', true];
+
