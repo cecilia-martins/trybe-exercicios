0e1cf40c23a7d6a1af2f319af786990d6bf43305
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-12-02 12:07:03 -0300

    add exercicio 3 - gabarito

--- ciencia-da-computacao/introducao-a-python/dia-1-aprendendo-python/ex03.py
+++ ciencia-da-computacao/introducao-a-python/dia-1-aprendendo-python/ex03.py
@@ -1,7 +1,15 @@
+# def imprime_asteriscos(n: int):
+#     asteriscos = '*'
+#     if n > 1:
+#         return n * asteriscos
+
+
+# print(imprime_asteriscos(7))
+
+# solução do gabarito
 def imprime_asteriscos(n: int):
-    asteriscos = '*'
-    if n > 1:
-        return n * asteriscos
+    for row in range(n):
+        print(n * '*')
 
 
-print(imprime_asteriscos(7))
+imprime_asteriscos(7)
