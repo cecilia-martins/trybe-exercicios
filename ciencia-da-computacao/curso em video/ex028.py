3c08d67903d3b1112171bf21457d01090b9fdc0f
Cecília Martins <cecilia_lmartins@hotmail.com>
2023-01-03 15:57:40 -0300

    add exercicio #028

---
+++ ciencia-da-computacao/curso em video/ex028.py
@@ -0,0 +1,10 @@
+from random import randint
+
+aleatorio = randint(0, 5)
+
+numero = int(input('Digite um número entre 0 e 5: '))
+
+if numero == aleatorio: 
+    print('Você venceu!!')
+else:
+    print('Você perdeu!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
