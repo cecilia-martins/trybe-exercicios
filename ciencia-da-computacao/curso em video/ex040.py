79e238e7e1edd85dd4329748dc66d996604e5035
Cecília Martins <cecilia_lmartins@hotmail.com>
2023-01-04 15:14:48 -0300

    add exercicio #040

---
+++ ciencia-da-computacao/curso em video/ex040.py
@@ -0,0 +1,13 @@
+nota_1 = float(input('Digite a primeira nota: '))
+nota_2 = float(input('Digite a segunda nota: '))
+
+media = (nota_1 + nota_2) // 2
+
+if media < 5:
+    print('REPROVADO!')
+elif media == 5 or media < 6.9:
+    print('RECUPERAÇÃO!')
+elif media >= 7:
+    print('APROVADO!!!!!')
+else:
+    print('SE NÃO É ISSO :)')
