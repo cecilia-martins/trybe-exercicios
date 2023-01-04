49a78fddd2582120ccbcdbbaf1e29bffce0395bd
Cecília Martins <cecilia_lmartins@hotmail.com>
2023-01-04 15:56:59 -0300

    add exercicio #043

---
+++ ciencia-da-computacao/curso em video/ex043.py
@@ -0,0 +1,16 @@
+peso = float(input('Qual seu peso? '))
+altura = float(input('Qual sua altura? '))
+
+imc = peso / (altura * altura)
+
+print('Seu imc é de {:.2f}'.format(imc))
+if imc < 18.5:
+    print('ABAIXO DO PESO')
+elif 18.5 < imc < 25:
+    print('PESO IDEAL')
+elif 25 < imc < 30:
+    print('SOBREPESO')
+elif 30 < imc < 40:
+    print('OBESIDADE')
+else: 
+    print('OBESIDADE MÓRBIDA')
