7c568fc56e30d8a79a4530b1312a0bbc362194af
Cecília Martins <cecilia_lmartins@hotmail.com>
2023-01-09 09:29:15 -0300

    add exercicio #58

--- ciencia-da-computacao/curso em video/ex058.py
+++ ciencia-da-computacao/curso em video/ex058.py
@@ -2,17 +2,38 @@ from random import randint
 
 aleatorio = randint(0, 10)  # gera um número "aleatorio" entre 0 e 5
 
-numero = int(input('Digite um número entre 0 e 10: '))
-contador = 1
+# numero = int(input('Digite um número entre 0 e 10: '))
+# contador = 1
 
-while numero != aleatorio:
-    # print('Você venceu!!')
-    contador += 1
-    numero = int(input('Digite um número entre 0 e 10: '))
-else:
-    print('Você acertou!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+# while numero != aleatorio:
+#     # print('Você venceu!!')
+#     contador += 1
+#     numero = int(input('Digite um número entre 0 e 10: '))
+# else:
+#     print('Você acertou!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
-if contador == 1:
-    print(f'Acertou depois de {contador} tentativa')
+# if contador == 1:
+#     print(f'Acertou depois de {contador} tentativa')
+# else:
+#     print(f'Acertou depois de {contador} tentativas')
+
+acertou = False
+palpite = 0
+
+while not acertou:
+    palpite += 1
+    jogador = int(input('Digite seu palpite entre 0 e 10: '))
+    if jogador == aleatorio:
+        acertou = True
+    else:
+        if jogador < aleatorio:
+            print('Mais...')
+        elif jogador > aleatorio:
+            print('Menos...')
+
+if palpite == 1:
+    print(f'Acertou depois de {palpite} tentativa')
 else:
-    print(f'Acertou depois de {contador} tentativas')
+    print(f'Acertou depois de {palpite} tentativas')
+
+print('AÊ!!')
